d42c88e34e39895b0cc99b436307755464d644e4
Utsav Karan <ukaran11@gmail.com>
2021-11-17 23:54:45 +0530

    Utsav | Added PostList comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import { Admin, Resource } from 'react-admin';
+import restProvider from 'ra-data-simple-rest';
+import PostList from './components/PostList';
+
 function App() {
   return (
-    <div className="App">
-      Hello 
-    </div>
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+      <Resource name="posts" list={PostList} />
+    </Admin>
   );
 }
 
